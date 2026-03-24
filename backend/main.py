087f8e353874090ae42a521d9acc5a56aac8fcd3
Siddharth Jain <jainsiddharth252@gmail.com>
2026-03-25 00:46:38 +0530

    test

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://airwriting2.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
